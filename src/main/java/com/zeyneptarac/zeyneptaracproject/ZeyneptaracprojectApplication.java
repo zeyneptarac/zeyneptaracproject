b28a63fe472ff905e09c66e79bef828f1669c61a
zeyneptarac <zeynp9827@gmail.com>
2023-02-22 09:51:56 +0300

    Initial commit

---
+++ src/main/java/com/zeyneptarac/zeyneptaracproject/ZeyneptaracprojectApplication.java
@@ -0,0 +1,13 @@
+package com.zeyneptarac.zeyneptaracproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeyneptaracprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeyneptaracprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeyneptarac/zeyneptaracproject/ZeyneptaracprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeyneptarac.zeyneptaracproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeyneptaracprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
